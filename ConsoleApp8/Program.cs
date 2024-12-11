92fd6b6b6f8dcb5ff5da18a00c6c8d080face69a
Mustafojon <Mustafojon@DESKTOP-P6KNFPB>
2024-12-11 15:25:37 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+class Program
+{
+    static void Main()
+    {
+        List<int> list = new List<int>();
+        // Ro'yxatga elementlar qo'shish
+        list.Add(10);
+        list.Add(20);
+        list.Add(30);
+        // Ro'yxatdagi elementlarni chop etish
+        Console.WriteLine("Ro'yxat elementlari:");
+        foreach (int item in list)
+        {
+            Console.WriteLine(item);
+        }
+        // Ro'yxatdan elementni o'chirish
+        list.Remove(20);
+        Console.WriteLine("Element o'chirilganidan keyin:");
+        foreach (int item in list)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
